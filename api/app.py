48bf74197db24effa6a2d611e1d7ec63cb1db99a
Jake <jakemackerracher@gmail.com>
2024-09-05 10:47:14 +0100

    Added support for .env

--- api/app.py
+++ api/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from base import db, migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 db.init_app(app)
 migrate.init_app(app, db)
