52d8075e5bdd240dad1560e74cd30455a677577e
Jake <jakemackerracher@gmail.com>
2024-09-06 19:51:33 +0100

    Created pytest client fixture

--- api/tests/test_app_home.py
+++ api/tests/test_app_home.py
@@ -1,8 +1,6 @@
 import pytest
-from app import app
 
-def test_app_home_endpoint():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert b'Hello, Flask!' in response.data
+def test_app_home_endpoint(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, Flask!' in response.data
