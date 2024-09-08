65b272327777951faa867561a4820cb21510ce85
Jake <jakemackerracher@gmail.com>
2024-09-08 23:23:03 +0100

    Styled Not Found page

--- app/src/pages/NotFound.js
+++ app/src/pages/NotFound.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./NotFound.module.scss";
 
 function NotFound() {
-  return <h2>404 - Not Found</h2>;
+  return (
+    <div className={styles.wrapper}>
+      <h2>404 - Not Found</h2>
+    </div>
+  );
 }
 
 export default NotFound;
