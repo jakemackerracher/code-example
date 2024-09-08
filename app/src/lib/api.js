d31632049653996ede7eef829ff57689ccca4c7d
Jake <jakemackerracher@gmail.com>
2024-09-08 23:25:00 +0100

    Updated api baseURL within React app

--- app/src/lib/api.js
+++ app/src/lib/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process?.env?.REACT_APP_API_URL,
+  // TODO: fix env variables not working
+  baseURL: "http://127.0.0.1:5000/",
 });
