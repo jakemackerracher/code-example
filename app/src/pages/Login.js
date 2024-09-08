a3eccbb28e8f04ca73863e2f5bff5ba90aeda92f
Jake <jakemackerracher@gmail.com>
2024-09-08 22:27:50 +0100

    Created base login page

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Login.module.scss";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <h2>Login Page</h2>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Ensure we have the required fields
+    if (!email && !password) {
+      setError("Email and password required");
+      return;
+    }
+
+    // TODO: connect submit to api
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    // Clear form
+    setEmail("");
+    setPassword("");
+    setError("");
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required={true}
+        />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required={true}
+        />
+        <button type="submit">Login</button>
+        <div className={styles.register_here}>
+          Don't have an account yet? Create one <Link to="/register">here</Link>
+          .
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
