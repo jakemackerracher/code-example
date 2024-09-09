ca41b6fb646d9bbafb85ff832ebaf8c2af03d171
Jake <jakemackerracher@gmail.com>
2024-09-09 01:24:37 +0100

    Updated home to show user attributes

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useAuth } from "../authContext";
+import styles from "./Home.module.scss";
 
 function Home() {
-  return <h2>Home Page</h2>;
+  const { authState } = useAuth();
+
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.welcome}>
+        Hello {authState?.name || "Guest"}, welcome to my code-example.
+      </h2>
+      {authState && (
+        <div className={styles.user_attributes}>
+          <h3>User Attributes</h3>
+          <div>
+            Name: {authState?.name}
+            {authState?.is_admin ? " [ADMIN]" : null}
+          </div>
+          <div>Email: {authState?.email}</div>
+          <div>Created: {authState?.created_at}</div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
